1b09b4c151c7fffbd1e7696a2d474d55c098b604
Manav Aswani <aswanimanav@gmail.com>
2020-09-04 17:26:38 +0530

    add files

---
+++ script.js
@@ -0,0 +1,97 @@
+var numSquares = 6;
+var colours = [];
+var pickedColour;
+var squares = document.querySelectorAll(".square");
+var colourDisplay = document.getElementById("colourDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.getElementById("reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+var modeButtons = document.querySelectorAll(".mode");
+
+init();
+
+function init() {
+	setupModeButtons();	 
+	setupSquares();
+	reset();
+}
+
+function setupModeButtons() {
+	for(var i = 0; i < modeButtons.length; i++) {
+		modeButtons[i].addEventListener("click", function(){
+			modeButtons[0].classList.remove("selected");
+			modeButtons[1].classList.remove("selected");
+			this.classList.add("selected");
+			numSquares = (this.textContent === "Easy") ? 3 : 6;
+			reset();
+		});
+	}
+}
+
+function setupSquares() {
+	for(var i = 0; i < squares.length; i++) {
+		squares[i].addEventListener("click", function(){
+			var clickedColour = this.style.backgroundColor;
+			if(clickedColour === pickedColour) {
+				messageDisplay.textContent = "Correct!";
+				resetButton.textContent = "Play Again?";
+				changeColours(clickedColour);
+				h1.style.backgroundColor = clickedColour;
+			} else {
+				this.style.backgroundColor = "#232323";
+				messageDisplay.textContent = "Try Again";
+			}
+		});
+	}
+}
+
+
+function reset() {
+	colours = generateRandomColours(numSquares);
+	pickedColour = pickColour();
+	colourDisplay.textContent = pickedColour;
+	for(var i = 0; i < squares.length; i++) {
+		if(colours[i]) {
+			squares[i].style.display = "block"; 
+			squares[i].style.backgroundColor = colours[i];
+		} else {
+			squares[i].style.display = "none"; 
+		}
+	}
+	h1.style.backgroundColor = "steelblue";
+	messageDisplay.textContent = "";
+	resetButton.textContent = "New Colours";
+}
+
+
+resetButton.addEventListener("click", function(){
+	reset();
+});
+
+
+function changeColours(colour) {
+	for(var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = colour;
+	}
+}
+
+function pickColour() {
+	var random = Math.floor(Math.random() * colours.length);
+	return colours[random];
+}
+
+function generateRandomColours(num) {
+	var arr = [];
+	for(var i = 0; i < num; i++) {
+		var red = Math.floor(Math.random() * 256);
+		var green = Math.floor(Math.random() * 256);
+		var blue = Math.floor(Math.random() * 256);
+		arr.push("rgb(" + red + ", " + green + ", " + blue + ")");
+	}
+	return arr;
+}
+
+
+
